1fea358e21ebc3fdbf47d1c8cd4e11de7d6db0fa
Zino Holwerda <z.holwerda@gmail.com>
2023-01-12 17:00:13 +0100

    added first working versino

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@
 #![feature(abi_avr_interrupt)]
 
 mod millis;
+mod seven_segment_display;
 
+use hal::delay_us;
 use panic_halt as _;
 use dht11::Dht11;
+use crate::seven_segment_display::SevenSeg;
 use arduino_hal as hal;
 use crate::millis::{millis, millis_init};
 
@@ -19,22 +22,37 @@ unsafe fn main() -> ! {
 
     millis_init(dp.TC0);
 
-    let pin = pins.d4.into_opendrain();
-    let mut led = pins.d13.into_output();
+    let pin = pins.a1.into_opendrain();
     let mut dht11 = Dht11::new(pin);
     let mut delay = hal::Delay::new();
     let mut time_now: u32 = 0;
+    // Pins for the seven segment display.
+    // digitPins: 
+    // segmentPins: 
+    let num_1 = pins.d13.into_output().downgrade();
+    let num_2 = pins.d12.into_output().downgrade();
+    let num_3 = pins.d11.into_output().downgrade();
+    let num_4 = pins.d10.into_output().downgrade();
+    let seg_a = pins.d9.into_output().downgrade();
+    let seg_b = pins.d8.into_output().downgrade();
+    let seg_c = pins.d7.into_output().downgrade();
+    let seg_d = pins.d6.into_output().downgrade();
+    let seg_e = pins.d5.into_output().downgrade();
+    let seg_f = pins.d4.into_output().downgrade();
+    let seg_g = pins.d3.into_output().downgrade();
+    let seg_dp = pins.d2.into_output().downgrade();
+    let mut seven_segment_display = SevenSeg::new(0, num_1, num_2, num_3, num_4, seg_a, seg_b, seg_c, seg_d, seg_e, seg_f, seg_g, seg_dp);
     
     loop {
-        arduino_hal::delay_ms(1000);
         let time = millis();
         if time - time_now > 1000 {
             time_now = time;
-            led.toggle();
             let measurement = dht11.perform_measurement(&mut delay).unwrap();
             ufmt::uwriteln!(&mut serial, "{}.{}", measurement.temperature/10, 
                 measurement.temperature%10).unwrap();
-            led.toggle();
+            seven_segment_display.set_value(measurement.temperature as u16);
         }
+        delay_us(10);
+        seven_segment_display.display()
     }
 }
--- src/seven_segment_display.rs
+++ src/seven_segment_display.rs
@@ -1,8 +1,328 @@
 extern crate arduino_hal;
 
-use arduino_hal::port::{Pin, mode}
+use arduino_hal::{port::{Pin, mode}, delay_us};
 
-struct SevenSegmentDisplayPart<PINA, PINB, PINB, PINC, PIND, PINE, PINF, PING, PINDP> {
-    //pub a: Pin<mode:: 
+
+/// A structure representing the 7 segments of a 7-segment display
+pub struct SevenSeg {
+    value: u16,
+    num_1: Pin<mode::Output>,
+    num_2: Pin<mode::Output>,
+    num_3: Pin<mode::Output>,
+    num_4: Pin<mode::Output>,
+    seg_a: Pin<mode::Output>,
+    seg_b: Pin<mode::Output>,
+    seg_c: Pin<mode::Output>,
+    seg_d: Pin<mode::Output>,
+    seg_e: Pin<mode::Output>,
+    seg_f: Pin<mode::Output>,
+    seg_g: Pin<mode::Output>,
+    seg_dp: Pin<mode::Output>
 }
 
+impl SevenSeg
+{
+    pub fn new(
+        value: u16,
+        num_1: Pin<mode::Output>,
+        num_2: Pin<mode::Output>,
+        num_3: Pin<mode::Output>,
+        num_4: Pin<mode::Output>,
+        seg_a: Pin<mode::Output>,
+        seg_e: Pin<mode::Output>,
+        seg_c: Pin<mode::Output>,
+        seg_g: Pin<mode::Output>,
+        seg_b: Pin<mode::Output>,
+        seg_f: Pin<mode::Output>,
+        seg_d: Pin<mode::Output>,
+        seg_dp: Pin<mode::Output>
+    ) -> Self {
+        Self {
+            value,
+            num_1,
+            num_2,
+            num_3,
+            num_4,
+            seg_a,
+            seg_e,
+            seg_c,
+            seg_g,
+            seg_b,
+            seg_f,
+            seg_d,
+            seg_dp,
+        }
+    }
+
+    /// Disable the 7-segment display by pulling all GPIOs low
+    pub fn clear(&mut self) {
+        self.seg_a(false);
+        self.seg_b(false);
+        self.seg_c(false);
+        self.seg_d(false);
+        self.seg_e(false);
+        self.seg_f(false);
+        self.seg_g(false);
+        self.seg_dp(false)
+    }
+
+    // Enable or disable segment `a` according to the `state`
+    pub fn num_1(&mut self, state: bool) {
+        if state {
+            self.num_1.set_high()
+        } else {
+            self.num_1.set_low()
+        }
+    }
+
+    /// Enable or disable segment `a` according to the `state`
+    pub fn num_2(&mut self, state: bool) {
+        if state {
+            self.num_2.set_high()
+        } else {
+            self.num_2.set_low()
+        }
+    }
+
+    /// Enable or disable segment `a` according to the `state`
+    pub fn num_3(&mut self, state: bool) {
+        if state {
+            self.num_3.set_high()
+        } else {
+            self.num_3.set_low()
+        }
+    }
+
+    /// Enable or disable segment `a` according to the `state`
+    pub fn num_4(&mut self, state: bool) {
+        if state {
+            self.num_4.set_high()
+        } else {
+            self.num_4.set_low()
+        }
+    }
+
+    /// Enable or disable segment `a` according to the `state`
+    pub fn seg_a(&mut self, state: bool) {
+        if state {
+            self.seg_a.set_high()
+        } else {
+            self.seg_a.set_low()
+        }
+    }
+
+    /// Enable or disable segment `b` according to the `state`
+    pub fn seg_b(&mut self, state: bool) {
+        if state {
+            self.seg_b.set_high()
+        } else {
+            self.seg_b.set_low()
+        }
+    }
+
+    /// Enable or disable segment `c` according to the `state`
+    pub fn seg_c(&mut self, state: bool) {
+        if state {
+            self.seg_c.set_high()
+        } else {
+            self.seg_c.set_low()
+        }
+    }
+
+    /// Enable or disable segment `d` according to the `state`
+    pub fn seg_d(&mut self, state: bool) {
+        if state {
+            self.seg_d.set_high()
+        } else {
+            self.seg_d.set_low()
+        }
+    }
+
+    /// Enable or disable segment `e` according to the `state`
+    pub fn seg_e(&mut self, state: bool) {
+        if state {
+            self.seg_e.set_high()
+        } else {
+            self.seg_e.set_low()
+        }
+    }
+
+    /// Enable or disable segment `f` according to the `state`
+    pub fn seg_f(&mut self, state: bool) {
+        if state {
+            self.seg_f.set_high()
+        } else {
+            self.seg_f.set_low()
+        }
+    }
+
+    /// Enable or disable segment `g` according to the `state`
+    pub fn seg_g(&mut self, state: bool) {
+        if state {
+            self.seg_g.set_high()
+        } else {
+            self.seg_g.set_low()
+        }
+    }
+
+
+    /// Enable or disable segment `g` according to the `state`
+    pub fn seg_dp(&mut self, state: bool) {
+        if state {
+            self.seg_dp.set_high()
+        } else {
+            self.seg_dp.set_low()
+        }
+    }
+
+    /// Enable or disable segment `g` according to the `state`
+    pub fn set_value(&mut self, value: u16) {
+        self.value = value
+    }
+
+    /// Display the digit specified in `num`. Supported are all values in the hexadecimal system,
+    /// that is `0` through `9` and `A` through `F`. Any other value will turn off the display.
+    pub fn display(&mut self) {
+        let mut rest = self.value;
+        for i in 1..=4 {
+            match i {
+                4 => {
+                    self.num_2(true);
+                    self.num_3(true);
+                    self.num_4(true);
+                    self.seg_dp(false);
+                    self.num_1(false);
+                }
+                3 => {
+                    self.num_1(true);
+                    self.num_3(true);
+                    self.num_4(true);
+                    self.seg_dp(false);
+                    self.num_2(false);
+                }
+                2 => {
+                    self.num_1(true);
+                    self.num_2(true);
+                    self.num_4(true);
+                    self.seg_dp(true);
+                    self.num_3(false);
+                }
+                1 => {
+                    self.num_1(true);
+                    self.num_2(true);
+                    self.num_3(true);
+                    self.seg_dp(false);
+                    self.num_4(false);
+                }
+                _ => {}
+            }
+            //delay_us(100);
+            match rest % 10 {
+                0 => {
+                    self.seg_a(true);
+                    self.seg_b(true);
+                    self.seg_c(true);
+                    self.seg_d(true);
+                    self.seg_e(true);
+                    self.seg_f(true);
+                    self.seg_g(false)
+                }
+                1 => {
+                    self.seg_a(false);
+                    self.seg_b(false);
+                    self.seg_c(false);
+                    self.seg_d(false);
+                    self.seg_e(true);
+                    self.seg_f(true);
+                    self.seg_g(false)
+                }
+                2 => {
+                    self.seg_a(true);
+                    self.seg_b(true);
+                    self.seg_c(false);
+                    self.seg_d(true);
+                    self.seg_e(true);
+                    self.seg_f(false);
+                    self.seg_g(true)
+                }
+                3 => {
+                    self.seg_a(true);
+                    self.seg_b(false);
+                    self.seg_c(false);
+                    self.seg_d(true);
+                    self.seg_e(true);
+                    self.seg_f(true);
+                    self.seg_g(true)
+                }
+                4 => {
+                    self.seg_a(false);
+                    self.seg_b(false);
+                    self.seg_c(true);
+                    self.seg_d(false);
+                    self.seg_e(true);
+                    self.seg_f(true);
+                    self.seg_g(true)
+                }
+                5 => {
+                    self.seg_a(true);
+                    self.seg_b(false);
+                    self.seg_c(true);
+                    self.seg_d(true);
+                    self.seg_e(false);
+                    self.seg_f(true);
+                    self.seg_g(true)
+                }
+                6 => {
+                    self.seg_a(true);
+                    self.seg_b(true);
+                    self.seg_c(true);
+                    self.seg_d(true);
+                    self.seg_e(false);
+                    self.seg_f(true);
+                    self.seg_g(true)
+                }
+                7 => {
+                    self.seg_a(false);
+                    self.seg_b(false);
+                    self.seg_c(false);
+                    self.seg_d(true);
+                    self.seg_e(true);
+                    self.seg_f(true);
+                    self.seg_g(false)
+                }
+                8 => {
+                    self.seg_a(true);
+                    self.seg_b(true);
+                    self.seg_c(true);
+                    self.seg_d(true);
+                    self.seg_e(true);
+                    self.seg_f(true);
+                    self.seg_g(true)
+                }
+                9 => {
+                    self.seg_a(true);
+                    self.seg_b(false);
+                    self.seg_c(true);
+                    self.seg_d(true);
+                    self.seg_e(true);
+                    self.seg_f(true);
+                    self.seg_g(true)
+                }
+                10 => {
+                    self.seg_a(false);
+                    self.seg_b(true);
+                    self.seg_c(true);
+                    self.seg_d(true);
+                    self.seg_e(true);
+                    self.seg_f(true);
+                    self.seg_g(true)
+                }
+                _ => self.clear()
+            }
+            rest = rest / 10;
+            delay_us(2000);
+            //self.clear();
+            //delay_us(50);
+        }
+    }
+}
